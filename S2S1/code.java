3ba66ed7b675e04a9b9d366466305ebbe0db1670
fabiooo000 <fabio.ventura.0602s@gmail.com>
2025-08-22 09:40:37 -0500

    Update code.java

--- S2S1/code.java
+++ S2S1/code.java
@@ -1,1 +1,20 @@
-
+public class Insercion {
+    
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 1; i < n; i++) {
+            int clave = arr[i];  // Elemento actual a insertar
+            int j = i - 1;
+            
+            // Mover elementos mayores que la clave una posición adelante
+            while (j >= 0 && arr[j] > clave) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            
+            // Insertar la clave en su posición correcta
+            arr[j + 1] = clave;
+        }
+    }
+}
