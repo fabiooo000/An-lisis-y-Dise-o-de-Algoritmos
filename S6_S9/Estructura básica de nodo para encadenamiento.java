dca3766777ea71be077ad308250b1a33bf1efb38
fabiooo000 <fabio.ventura.0602s@gmail.com>
2025-10-16 14:18:55 -0500

    Create Estructura básica de nodo para encadenamiento.java

---
+++ S6_S9/Estructura básica de nodo para encadenamiento.java
@@ -0,0 +1,12 @@
+// Clase auxiliar para representar un nodo en la lista enlazada de colisiones
+class HashNode {
+    int key;
+    String value;
+    HashNode next;
+
+    HashNode(int key, String value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
